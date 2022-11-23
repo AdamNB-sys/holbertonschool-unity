97ab86fbda59c906fd0e616051d8e3f8769a90f7
AdamNB-sys <a1adnat@gmail.com>
2022-11-22 21:20:39 -0600

    Options works now cuz i learned how to spell

--- 0x00-unity-assets_ui/Assets/Scripts/OptionsMenu.cs
+++ 0x00-unity-assets_ui/Assets/Scripts/OptionsMenu.cs
@@ -1,17 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngie.SceneManagement;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class OptionsMenu : MonoBehaviour
 {
     public void Back()
-    {
-        goBack();
-    }
-
-    public void goBack()
     {
         SceneManager.LoadScene(0);
     }
