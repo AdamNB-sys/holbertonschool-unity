a7e5841765f3bfcbf87cbf03b054ed5ad4b5f2f6
AdamNB-sys <a1adnat@gmail.com>
2022-12-15 22:16:34 -0600

    Alright now we're getting somewhere

---
+++ 0x00-unity-animation/Assets/Scripts/AnimationController.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationController : MonoBehaviour
+{
+    static Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D))
+        {
+            animator.SetBool("isRunning", true);
+            animator.SetBool("isIdle", false);
+        }
+        else
+        {
+            animator.SetBool("isRunning", false);
+            animator.SetBool("isIdle", true);
+        }
+
+        if (Input.GetKeyDown("space"))
+        {
+            animator.SetTrigger("isJumping");
+        }
+    }
+}
